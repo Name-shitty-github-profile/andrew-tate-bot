4fbd51c2ba3d500b960d3010befb966a1a96d108
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-08-07 19:17:50 -0400

    Update links.py

--- cogs/links.py
+++ cogs/links.py
@@ -20,9 +20,13 @@ class Links(commands.Cog):
   async def donateslash(self, interaction: nextcord.Interaction):
     await interaction.response.send_message(content="Here's the creator buy me a coffee, not for pussy btw.\nhttps://www.buymeacoffee.com/notnoemie")
 
-  @nextcord.slash_command(name = "nerd", description = "This show the place to donate for supporting the discord bot.")
+  @nextcord.slash_command(name = "nerd", description = "This show the source code of the bot, for the nerds.")
   async def nerdslash(self, interaction: nextcord.Interaction):
-    await interaction.response.send_message(content="Hello nerd, no one loves you and no one will never love you.\nHere's your fucking code nerd.\n")
+    await interaction.response.send_message(content="Hello nerd, no one loves you and no one will never love you.\nHere's your fucking code nerd.\nhttps://github.com/Name-shitty-github-profile/andrew-tate-bot")
+
+  @nextcord.slash_command(name = "add me", description = "This show a link to add me.")
+  async def addmeslash(self, interaction: nextcord.Interaction):
+    await interaction.response.send_message(embed=nextcord.Embed(title="Add me", description="[Click here to add me](https://discord.com/api/oauth2/authorize?client_id=1005589411259486259&permissions=8&scope=bot%20applications.commands)\nCan't click?\nHere's another way to add me : https://discordbotlist.com/bots/andrew-tate", color = 0x2ecc71))
 
 def setup(bot):
   bot.add_cog(Links(bot))
